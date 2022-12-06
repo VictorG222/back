90ebfd468f78c93f376b0be5574c70fb1a40d2c7
Fernando Sandoval Jr <85634577+Joker-280k@users.noreply.github.com>
2022-12-06 02:46:57 -0700

    Funcion SHOW para proyectos

--- front/app/Http/Livewire/ProyectoShow.php
+++ front/app/Http/Livewire/ProyectoShow.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class ProyectoShow extends Component
 {
+    public $idProyecto;
+    public function mount($id)
+    {
+        $this->idProyecto = $id;
+    }
+
     public function render()
     {
-        return view('livewire.proyecto-show');
+        $proyecto = Http::get('http://127.0.0.1:8001/api/proyectos/'.$this->idProyecto)->json();
+        return view('livewire.proyecto-show',compact('proyecto'));
     }
 }
