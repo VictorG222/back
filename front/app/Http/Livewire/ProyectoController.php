17dd0537c8cb9feeabec71ed93ced049f63286aa
Fernando Sandoval Jr <85634577+Joker-280k@users.noreply.github.com>
2022-12-06 02:38:19 -0700

    Tabla de Proyectos y logica del controlador

--- front/app/Http/Livewire/ProyectoController.php
+++ front/app/Http/Livewire/ProyectoController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class ProyectoController extends Component
 {
+    public $idProyecto;
+    public $listeners = ['delete'];
+
+
     public function render()
     {
-        return view('livewire.proyecto-controller');
+        $response = Http::get('http://127.0.0.1:8001/api/proyectos');
+        $proyectos = $response->json();
+        return view('livewire.proyecto-controller', compact('proyectos'));
     }
+
+    public function ConfirmarDelete($id)
+    {
+        $this->idProyecto = $id;
+        Http::delete('http://127.0.0.1:8001/api/proyectos/'.$this->idProyecto);
+
+    }
+
 }
