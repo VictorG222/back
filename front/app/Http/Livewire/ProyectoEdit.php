d9c9cf2d4ebbdd586f33530844e866f916774125
Fernando Sandoval Jr <85634577+Joker-280k@users.noreply.github.com>
2022-12-06 02:43:14 -0700

    Funcion EDIT de proyectos

--- front/app/Http/Livewire/ProyectoEdit.php
+++ front/app/Http/Livewire/ProyectoEdit.php
@@ -3,11 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class ProyectoEdit extends Component
 {
+
+    public $datos = [];
+    public $errores = [];
+    public $idProyecto;
+
+    public function mount($id)
+    {
+        $this->idProyecto = $id;
+        $this->datos = Http::get('http://127.0.0.1:8001/api/proyectos/' . $id)->json();
+    }
+
     public function render()
     {
         return view('livewire.proyecto-edit');
+    }
+
+    public function modificar()
+    {
+        $response = Http::withHeaders(['Accept'=>'Application/json'])
+        ->put('http://127.0.0.1:8001/api/proyectos/' . $this->idProyecto, $this->datos);
+        if ($response->successful()) {
+            // $this->emit('success', 'Se modifico con exito');
+            return redirect('/proyecto');
+        } else {
+            $this->errores = $response->json();
+        }
     }
 }
